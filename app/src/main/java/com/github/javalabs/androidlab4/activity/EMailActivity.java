d642774941a6ee76e3017bdaa0356904c37f10ae
Yegorov <yegorov0725@yandex.ru>
2016-02-22 01:55:57 +0300

    Add activities and layouts

---
+++ app/src/main/java/com/github/javalabs/androidlab4/activity/EMailActivity.java
@@ -0,0 +1,15 @@
+package com.github.javalabs.androidlab4.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.javalabs.androidlab4.R;
+
+public class EMailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email);
+    }
+}
---
+++ app/src/main/java/com/github/javalabs/androidlab4/activity/SmsActivity.java
@@ -0,0 +1,15 @@
+package com.github.javalabs.androidlab4.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.javalabs.androidlab4.R;
+
+public class SmsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms);
+    }
+}
